9e45d2748c7c2c8a0fa7e6ccb107e163290bff46
sam <144790923+tryxapp@users.noreply.github.com>
2025-07-02 14:46:48 +0700

    Update run.py

--- files/checker/tools/shortir/1/script/run.py
+++ files/checker/tools/shortir/1/script/run.py
@@ -2,7 +2,7 @@ import os
 
 def extract_logins(file_path):
     logins = {
-        'wordpress': [], 'ftp': [], 'cpanel': [], 'whm': [], 'plesk': [], 'drupal': [], 'cyberpanel': [], 'zpanel': [],
+        'wordpress': [], 'ftp': [], 'cpanel': [], 'whm': [], 'plesk': [], 'drupal': [], 'magento': [], 'opencart': [], 'bitrix': [], 'phpmyadmin': [], 'owncloud': [], 'cyberpanel': [], 'zpanel': [],
         'joomla': [], 'directadmin': [], 'web-admin': [], 'aapanel': [], 'clientarea': [], 'Cms-Website': [], 'sar': [],
         'smss': [], 'moodle': [], 'zcom': [], 'bigdata': [], 'chiangmaipao': [], 'th': [], 'id': [], 'gov': [], 'edu': [], 
         'br': [], 'vn': [], 'warning-acfs': [], 'radityadika': []
@@ -20,23 +20,33 @@ def extract_logins(file_path):
                         continue
                     if 'wp-admin' in line or 'wp-login.php' in line or 'wp-content' in line or 'wp-includes' in line:
                         logins['wordpress'].append(line)
-                    if 'ftp://' in line:
+                    if 'ftp://' in line or 'ftp.' in line:
                         logins['ftp'].append(line)
                     if ':2082' in line or ':2083' in line or '/cpanel.' in line:
                         logins['cpanel'].append(line)
                     if ':2086' in line or ':2087' in line or '/whm.' in line:
                         logins['whm'].append(line)
-                    if '/login_up.php' in line or ':8443' in line:
+                    if 'login_up.php' in line or ':8443' in line:
                         logins['plesk'].append(line)
                     if '/user/login' in line or 'drupal' in line or '/core/' in line:
                         logins['drupal'].append(line)
+                    if '/admin/' in line:
+                        logins['magento'].append(line)
+                    if '/admin/index.php' in line:
+                        logins['opencart'].append(line)
+                    if '/bitrix/admin/' in line:
+                        logins['bitrix'].append(line)
+                    if '/phpmyadmin/' in line:
+                        logins['phpmyadmin'].append(line)
+                    if '/owncloud/' in line:
+                        logins['owncloud'].append(line)
                     if ':7080' in line:
                         logins['cyberpanel'].append(line)
                     if 'zpanel' in line:
                         logins['zpanel'].append(line)
-                    if '/administrator' in line or 'joomla' in line or 'com_content' in line: 
+                    if '/administrator' in line or 'joomla' in line or 'com_content' in line or 'administrator/index.php' in line: 
                         logins['joomla'].append(line)
-                    if ':2222' in line:
+                    if ':2222' in line or ':2222/evo/login' in line:
                         logins['directadmin'].append(line)
                     if '/web-admin' in line:
                         logins['web-admin'].append(line)
@@ -50,7 +60,7 @@ def extract_logins(file_path):
                         logins['sar'].append(line)
                     if 'smss.' in line:
                         logins['smss'].append(line)
-                    if 'moodle.' in line:
+                    if 'moodle.' in line or '/login/index.php' in line:
                         logins['moodle'].append(line)
                     if '.z.com' in line:
                         logins['zcom'].append(line)
